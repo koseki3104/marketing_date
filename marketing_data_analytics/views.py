38561fd9e441da57e7c49455fdb49f8605a071b8
koseki3104 <satoshi4500sports@icloud.com>
2023-07-24 10:08:04 +0900

    散布図の出力

--- marketing_data_analytics/views.py
+++ marketing_data_analytics/views.py
@@ -1,11 +1,30 @@
+import matplotlib.pyplot as plt
 from django.shortcuts import render
 from openpyxl import Workbook, load_workbook
+from openpyxl.drawing.image import Image
 from openpyxl.styles import PatternFill
 import pandas as pd
 import os
+import io
 from django.http import HttpResponse
 from django.conf import settings
 from .models import Review
+
+def create_scatter_plot(x_data, y_data, x_label, y_label):
+    fig, ax = plt.subplots()
+    ax.scatter(x_data, y_data)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    ax.set_title(f"{x_label} vs {y_label}")
+
+    # 画像を保存
+    img_data = io.BytesIO()
+    plt.savefig(img_data, format='png')
+    plt.close()
+
+    img_data.seek(0)  # ファイルポインタを先頭に戻す
+
+    return img_data
 
 def top_page(request):
     return render(request, 'top_page.html')
@@ -61,60 +80,71 @@ def export_to_excel(request):
     # Excelファイルに平均値と相関行列を出力（ファイル名を変更）
     file_path = os.path.join(settings.MEDIA_ROOT, 'data_analytics.xlsx')
     with pd.ExcelWriter(file_path, engine='openpyxl') as writer:
-        average_data.to_excel(writer, sheet_name='Average')  # Corrected here (index=True is used by default)
+        average_data.to_excel(writer, sheet_name='Average')
         correlation_matrix.to_excel(writer, sheet_name='CorrelationMatrix')
 
-    # 相関行列のシートを開いてセルの色を設定
-    book = load_workbook(file_path)
-    sheet = book['CorrelationMatrix']
+        # 散布図を追加していく処理
+        scatter_plots = {
+            '料理の満足度': numerical_data_df['food_satisfaction'],
+            '価格の満足度': numerical_data_df['price_satisfaction'],
+            '店の雰囲気': numerical_data_df['ambience_satisfaction'],
+            '接客態度': numerical_data_df['service_satisfaction'],
+        }
 
-    # セルの色を設定するためのカスタム関数
-    def set_cell_color(cell, color):
-        cell.fill = PatternFill(start_color=color, end_color=color, fill_type="solid")
+        for label, data in scatter_plots.items():
+            img_data = create_scatter_plot(numerical_data_df['overall_satisfaction'], data, 'Overall Satisfaction', label)
+            # 新しいWorksheetを作成してそこに散布図を追加
+            scatter_sheet = writer.book.create_sheet(title=f'{label}_vs_Overall_Satisfaction')
+            scatter_sheet.add_image(Image(img_data), 'A1')
 
-    for row in sheet.iter_rows(min_row=2, max_row=correlation_matrix.shape[0]+1, min_col=2, max_col=correlation_matrix.shape[1]+1):
-        for cell in row:
-            value = cell.value
-            if value < -0.7:
-                set_cell_color(cell, "6495ED")  # 負の相関で濃い青に設定
-            elif value < -0.5:
-                set_cell_color(cell, "ADD8E6")  # 負の相関でもう少し濃い青に設定
-            elif value < -0.3:
-                set_cell_color(cell, "B0C4DE")  # 負の相関でさらに薄い青に設定
-            elif 0.3 <= value < 0.5:
-                set_cell_color(cell, "FF6347")  # 正の相関でオレンジ色に設定
-            elif 0.3 <= value < 0.7:
-                set_cell_color(cell, "FF4500")  # 正の相関で金色に設定
-            elif 0.7 <= value < 0.99:
-                set_cell_color(cell, "FF0000")  # 正の相関でダークオレンジ色に設定
+        # 相関行列のシートを開いてセルの色を設定
+        sheet = writer.book['CorrelationMatrix']
 
-    week_correlation_cell = sheet.cell(row=correlation_matrix.shape[0]+3, column=2)  
-    week_correlation_cell.value = "相関係数-0.3以上-0.5未満で弱い負の相関"
-    set_cell_color(week_correlation_cell, "B0C4DE")  # 負の相関でトマト色に設定
+        # セルの色を設定するためのカスタム関数
+        def set_cell_color(cell, color):
+            cell.fill = PatternFill(start_color=color, end_color=color, fill_type="solid")
 
-    normal_correlation_cell = sheet.cell(row=correlation_matrix.shape[0]+4, column=2)
-    normal_correlation_cell.value = "相関係数-0.5以上-0.7未満で普通の負の相関"
-    set_cell_color(normal_correlation_cell, "ADD8E6")  # 負の相関で橙赤に設定
+        for row in sheet.iter_rows(min_row=2, max_row=correlation_matrix.shape[0]+1, min_col=2, max_col=correlation_matrix.shape[1]+1):
+            for cell in row:
+                value = cell.value
+                if value < -0.7:
+                    set_cell_color(cell, "6495ED")  # 負の相関で濃い青に設定
+                elif value < -0.5:
+                    set_cell_color(cell, "ADD8E6")  # 負の相関でもう少し濃い青に設定
+                elif value < -0.3:
+                    set_cell_color(cell, "B0C4DE")  # 負の相関でさらに薄い青に設定
+                elif 0.3 <= value < 0.5:
+                    set_cell_color(cell, "FF6347")  # 正の相関でオレンジ色に設定
+                elif 0.3 <= value < 0.7:
+                    set_cell_color(cell, "FF4500")  # 正の相関で金色に設定
+                elif 0.7 <= value < 0.99:
+                    set_cell_color(cell, "FF0000")  # 正の相関でダークオレンジ色に設定
 
-    strong_correlation_cell = sheet.cell(row=correlation_matrix.shape[0]+5, column=2)
-    strong_correlation_cell.value = "相関係数-0.7以上で強い負の相関"
-    set_cell_color(strong_correlation_cell, "6495ED")  # 負の相関で濃い赤に設定
+        week_correlation_cell = sheet.cell(row=correlation_matrix.shape[0]+3, column=2)  
+        week_correlation_cell.value = "相関係数-0.3以上-0.5未満で弱い負の相関"
+        set_cell_color(week_correlation_cell, "B0C4DE")  # 負の相関でトマト色に設定
 
-    strong_positive_correlation_cell = sheet.cell(row=correlation_matrix.shape[0]+6, column=2)
-    strong_positive_correlation_cell.value = "相関係数0.7以上で強い正の相関"
-    set_cell_color(strong_positive_correlation_cell, "FF0000")  # 正の相関でダークオレンジ色に設定
+        normal_correlation_cell = sheet.cell(row=correlation_matrix.shape[0]+4, column=2)
+        normal_correlation_cell.value = "相関係数-0.5以上-0.7未満で普通の負の相関"
+        set_cell_color(normal_correlation_cell, "ADD8E6")  # 負の相関で橙赤に設定
 
-    normal_positive_correlation_cell = sheet.cell(row=correlation_matrix.shape[0]+7, column=2)
-    normal_positive_correlation_cell.value = "相関係数0.5以上0.7未満で普通の正の相関"
-    set_cell_color(normal_positive_correlation_cell, "FF4500")  # 正の相関でオレンジ色に設定
+        strong_correlation_cell = sheet.cell(row=correlation_matrix.shape[0]+5, column=2)
+        strong_correlation_cell.value = "相関係数-0.7以上で強い負の相関"
+        set_cell_color(strong_correlation_cell, "6495ED")  # 負の相関で濃い赤に設定
 
-    weak_positive_correlation_cell = sheet.cell(row=correlation_matrix.shape[0]+8, column=2)
-    weak_positive_correlation_cell.value = "相関係数0.3以上0.5未満で弱い正の相関"
-    set_cell_color(weak_positive_correlation_cell, "FF6347")  # 正の相関で金色に設定
+        strong_positive_correlation_cell = sheet.cell(row=correlation_matrix.shape[0]+6, column=2)
+        strong_positive_correlation_cell.value = "相関係数0.7以上で強い正の相関"
+        set_cell_color(strong_positive_correlation_cell, "FF0000")  # 正の相関でダークオレンジ色に設定
 
-    book.save(file_path)
+        normal_positive_correlation_cell = sheet.cell(row=correlation_matrix.shape[0]+7, column=2)
+        normal_positive_correlation_cell.value = "相関係数0.5以上0.7未満で普通の正の相関"
+        set_cell_color(normal_positive_correlation_cell, "FF4500")  # 正の相関でオレンジ色に設定
 
-    # ExcelファイルをダウンロードさせるためのResponseオブジェクトを作成
+        weak_positive_correlation_cell = sheet.cell(row=correlation_matrix.shape[0]+8, column=2)
+        weak_positive_correlation_cell.value = "相関係数0.3以上0.5未満で弱い正の相関"
+        set_cell_color(weak_positive_correlation_cell, "FF6347")  # 正の相関で金色に設定
+
+    # ファイルをダウンロードさせるResponseオブジェクトを作成
     with open(file_path, 'rb') as file:
         response = HttpResponse(file.read(), content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
         response['Content-Disposition'] = 'attachment; filename=data_analytics.xlsx'
