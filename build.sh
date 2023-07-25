5540098f992a241d48d3534e01ad842533e83356
koseki3104 <satoshi4500sports@icloud.com>
2023-07-25 21:53:57 +0900

    キャッシュの削除

--- build.sh
+++ build.sh
@@ -2,8 +2,17 @@
 # exit on error
 set -o errexit
 
+# キャッシュの削除
+rm -rf ~/.cache/matplotlib/*
+
+# 必要なパッケージのインストール
 pip install -r requirements.txt
 
+# 静的ファイルの収集
 python manage.py collectstatic --no-input
+
+# データベースのマイグレーション
 python manage.py migrate
+
+# スーパーユーザーの作成
 python manage.py superuser
