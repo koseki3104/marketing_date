2bf399dd6a93939b623520b6cb7ef12f9e7d75e2
koseki3104 <satoshi4500sports@icloud.com>
2023-07-29 09:44:15 +0900

    変更

--- marketing_date/settings.py
+++ marketing_date/settings.py
@@ -14,6 +14,7 @@ import environ
 from decouple import config
 from dj_database_url import parse as bdurl
 from pathlib import Path
+from dotenv import load_dotenv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -30,10 +31,11 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+load_dotenv()
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False# デプロイ時、Falseに修正
+DEBUG = True# デプロイ時、Falseに修正
 #DEBUG = True
 #if not DEBUG:
     #import django_heroku
