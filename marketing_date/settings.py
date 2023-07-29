956f91d5872a2b6c52089d2974e565c03107e65f
koseki3104 <satoshi4500sports@icloud.com>
2023-07-29 20:33:04 +0900

    テスト

--- marketing_date/settings.py
+++ marketing_date/settings.py
@@ -31,7 +31,7 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)gp&(qg=6!)+cw4m_&4md7eefe*tz_1qm^fq8eon!5j6@99o)a'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False# デプロイ時、Falseに修正
